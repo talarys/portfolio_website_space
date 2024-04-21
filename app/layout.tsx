746d54f47c1d8c4fcafbf6274cc0f5309e4fdeee
talarys <d.simon.ssx0@gmail.com>
2024-04-21 17:47:07 +0200

    Finished Starbackground

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StarCanvas from "@/components/StarBackground";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 			<body
 				className={`${inter.className} bg-bg overflow-y-scroll overflow-x-hidden`}
 			>
+				<StarCanvas />
 				{children}
 			</body>
 		</html>
