a7103a31e135fcaedd405b6c94e0808d64270428
talarys <d.simon.ssx0@gmail.com>
2024-04-22 00:45:29 +0200

    Temp footbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StarCanvas from "@/components/StarBackground";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 				<StarCanvas />
 				<Navbar />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
