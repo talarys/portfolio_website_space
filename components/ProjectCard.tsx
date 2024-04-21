3ea03fe37df05534ef2ce4c56521ef3f14b01ef6
talarys <d.simon.ssx0@gmail.com>
2024-04-22 00:55:45 +0200

    Added links to projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,28 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
 	src: string;
 	title: string;
 	description: string;
+	link: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, link }: Props) => {
 	return (
-		<div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-			<Image
-				src={src}
-				alt={title}
-				width={1000}
-				height={1000}
-				className="w-full object-contain"
-			/>
+		<Link href={link}>
+			<div className="h-full relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+				<Image
+					src={src}
+					alt={title}
+					width={1000}
+					height={1000}
+					className="w-full object-contain"
+				/>
 
-			<div className="relative p-4">
-				<h1 className="text-2xl font-semibold text-white">{title}</h1>
-				<p className="mt-2 text-gray-300">{description}</p>
+				<div className="relative p-4">
+					<h1 className="text-2xl font-semibold text-white">{title}</h1>
+					<p className="mt-2 text-gray-300">{description}</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 const Projects = () => {
 	return (
 		<div
-			className="flex flex-col items-center justify-center py-20 -z-10"
+			className="flex flex-col items-center justify-center py-20"
 			id="projects"
 		>
 			<h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
@@ -15,16 +15,19 @@ const Projects = () => {
 					src="/portfolio/Disney.png"
 					title="Disney+ Clone"
 					description="Disney+ Clone made using NextJS + TailwindCSS + Shadcn, with touch scrolling and search function using tmdb API"
+					link="https://disney-talarys.vercel.app"
 				/>
 				<ProjectCard
 					src="/portfolio/Medium.png"
 					title="Medium Clone"
 					description="Medium clone made with Next.js + TailwindCSS with Sanity as backend"
+					link="https://medium-2-0-liart.vercel.app"
 				/>
 				<ProjectCard
 					src="/portfolio/Netflix.png"
 					title="Space Themed Website"
 					description="Netflix Clone made using NextJS + TailwindCSS, implements Stripe for payments and firebase backend, with tmdb API."
+					link="https://netflix-talarys.vercel.app"
 				/>
 			</div>
 		</div>
